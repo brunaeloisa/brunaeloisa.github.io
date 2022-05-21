8bea61e1c88657cbafb759418dccb4fc3899eee7
brunaeloisa <69807930+brunaeloisa@users.noreply.github.com>
2022-05-21 10:26:35 -0300

    Update equalize.py

--- codigos/equalize.py
+++ codigos/equalize.py
@@ -6,26 +6,25 @@ cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
 if not (cap.isOpened()):
   print("Erro ao abrir a câmera.")
 
+height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+
+hist_size = 256
+hist_w, hist_h = 513, height
+bin_w = int(round(hist_w/hist_size))
+
 while(True):
     _, frame = cap.read()
     
     frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY) 
     frame_eq = cv2.equalizeHist(frame)
-    
-    cv2.imshow('normal', frame)
-    cv2.imshow('equalizado', frame_eq)
-    
-    hist_size = 256
-    hist_w, hist_h = 720, 540 #980, 720
-    bin_w = int(round(hist_w/hist_size))
-    
+
     # calcula histograma normal
     histImage = np.zeros((hist_h, hist_w), dtype=np.uint8)
     hist = cv2.calcHist([frame], [0], None, [hist_size], (0, 256)) 
     cv2.normalize(hist, hist, alpha=0, beta=hist_h, norm_type=cv2.NORM_MINMAX)
 
     # calcula histograma equalizado
-    histImage2 = np.zeros((hist_h, hist_w), dtype=np.uint8)
+    histImage_eq = np.zeros((hist_h, hist_w), dtype=np.uint8)
     hist_eq = cv2.calcHist([frame_eq], [0], None, [hist_size], (0, 256)) 
     cv2.normalize(hist_eq, hist_eq, alpha=0, beta=hist_h, norm_type=cv2.NORM_MINMAX)
 
@@ -33,12 +32,20 @@ while(True):
         cv2.line(histImage, (bin_w*i, hist_h - int(hist[i])),
             (bin_w*i, hist_h),
             255, thickness=2)
-        cv2.line(histImage2, (bin_w*i, hist_h - int(hist_eq[i])),
+        cv2.line(histImage_eq, (bin_w*i, hist_h - int(hist_eq[i])),
             (bin_w*i, hist_h),
             255, thickness=2)
+      
+    normal = cv2.hconcat([frame, histImage])
+    equalizado = cv2.hconcat([frame_eq, histImage_eq])
     
-    cv2.imshow('histograma normal', histImage)
-    cv2.imshow('histograma equalizado', histImage2)
+    cv2.imshow('normal', normal)
+    cv2.imshow('equalizado', equalizado)
 
-    if cv2.waitKey(1) & 0xFF == 27:
-        break
+    key = cv2.waitKey(10)
+    
+    if key == 27: 
+        break # encerra se apertar ESC
+    elif key == ord('s'):
+        cv2.imwrite('normal.png', normal)
+        cv2.imwrite('equalizado.png', equalizado)
