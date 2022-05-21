219aa2cde8ee3711a327258a5f27ccd1512f43df
brunaeloisa <69807930+brunaeloisa@users.noreply.github.com>
2022-05-21 18:34:39 -0300

    Update tiltshift.py

--- codigos/tiltshift.py
+++ codigos/tiltshift.py
@@ -1,16 +1,12 @@
 import cv2
 import numpy as np
 
-
 def on_trackbar(val):
     global img_final
 
-    slider_altura = cv2.getTrackbarPos(
-        'altura', 'Tiltshift')  # altura da região central
-    slider_centro = cv2.getTrackbarPos(
-        'centro', 'Tiltshift')  # posição vertical do centro
-    slider_decaimento = cv2.getTrackbarPos(
-        'decaimento', 'Tiltshift')  # intensidade de decaimento
+    slider_altura = cv2.getTrackbarPos('altura', 'Tiltshift')  # altura da região central
+    slider_centro = cv2.getTrackbarPos('centro', 'Tiltshift')  # posição vertical do centro
+    slider_decaimento = cv2.getTrackbarPos('decaimento', 'Tiltshift')  # intensidade de decaimento
 
     l1 = slider_centro - int(slider_altura/2)
     l2 = slider_centro + int(slider_altura/2)
@@ -47,12 +43,9 @@ img2 = img1.copy()
 img_final = img1.copy()
 
 cv2.namedWindow('Tiltshift')
-cv2.createTrackbar('altura', 'Tiltshift', slider_inicial,
-                   slider_max, on_trackbar)
-cv2.createTrackbar('centro', 'Tiltshift', slider_inicial,
-                   slider_max, on_trackbar)
-cv2.createTrackbar('decaimento', 'Tiltshift',
-                   slider_inicial, slider_max, on_trackbar)
+cv2.createTrackbar('altura', 'Tiltshift', slider_inicial, slider_max, on_trackbar)
+cv2.createTrackbar('centro', 'Tiltshift', slider_inicial, slider_max, on_trackbar)
+cv2.createTrackbar('decaimento', 'Tiltshift', slider_inicial, slider_max, on_trackbar)
 
 img_32f = np.float32(img2)
 for n in range(3):
