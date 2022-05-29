882c22195226f877f3a6899fbe32128279aa7f71
brunaeloisa <69807930+brunaeloisa@users.noreply.github.com>
2022-05-29 11:46:11 -0300

    Update equalize.py

--- codigos/equalize.py
+++ codigos/equalize.py
@@ -6,12 +6,15 @@ cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
 
 if not (cap.isOpened()):
     print("Erro ao abrir a câmera.")
-    sys.exit()
+    sys.exit() # encerra o programa
 
-height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
 
 hist_size = 256
-hist_w, hist_h = 513, height
+hist_w, hist_h = 512, 480
+hist_range = (0, 256)
+
 bin_w = int(round(hist_w/hist_size))
 
 while(True):
@@ -26,21 +29,17 @@ while(True):
 
     # calcula histograma normal
     histImage = np.zeros((hist_h, hist_w), dtype=np.uint8)
-    hist = cv2.calcHist([frame], [0], None, [hist_size], (0, 256)) 
+    hist = cv2.calcHist([frame], [0], None, [hist_size], hist_range) 
     cv2.normalize(hist, hist, alpha=0, beta=hist_h, norm_type=cv2.NORM_MINMAX)
 
     # calcula histograma equalizado
     histImage_eq = np.zeros((hist_h, hist_w), dtype=np.uint8)
-    hist_eq = cv2.calcHist([frame_eq], [0], None, [hist_size], (0, 256)) 
+    hist_eq = cv2.calcHist([frame_eq], [0], None, [hist_size], hist_range) 
     cv2.normalize(hist_eq, hist_eq, alpha=0, beta=hist_h, norm_type=cv2.NORM_MINMAX)
 
     for i in range(hist_size):
-        cv2.line(histImage, (bin_w*i, hist_h - int(hist[i])),
-            (bin_w*i, hist_h),
-            255, thickness=2)
-        cv2.line(histImage_eq, (bin_w*i, hist_h - int(hist_eq[i])),
-            (bin_w*i, hist_h),
-            255, thickness=2)
+        cv2.line(histImage, (bin_w*i, hist_h - int(hist[i])), (bin_w*i, hist_h), 255, thickness=2)
+        cv2.line(histImage_eq, (bin_w*i, hist_h - int(hist_eq[i])), (bin_w*i, hist_h), 255, thickness=2)
       
     normal = cv2.hconcat([frame, histImage])
     equalizado = cv2.hconcat([frame_eq, histImage_eq])
