59af529fb5621e24a2a92bd544dec5d5f305e829
brunaeloisa <69807930+brunaeloisa@users.noreply.github.com>
2022-05-29 11:51:44 -0300

    Update motiondetector.py

--- codigos/motiondetector.py
+++ codigos/motiondetector.py
@@ -20,11 +20,14 @@ while True:
     
     hist_size = 256
     hist_w, hist_h = 720, 540
+    hist_range = (0, 256)
+    
     bin_w = int(round(hist_w/hist_size))
+    
     histImage = np.zeros((hist_h, hist_w, 3), dtype=np.uint8)
 
     # calcula histograma atual (canal vermelho)
-    hist_atual = cv2.calcHist(bgr, [2], None, [hist_size], (0, 256))
+    hist_atual = cv2.calcHist(bgr, [2], None, [hist_size], hist_range)
     cv2.normalize(hist_atual, hist_atual, alpha=0, beta=hist_h, norm_type=cv2.NORM_MINMAX)
     
     if inicio:
@@ -32,11 +35,9 @@ while True:
         inicio = False
 
     for i in range(hist_size):
-        cv2.line(histImage, (bin_w*i, hist_h - int(hist_atual[i])),
-            (bin_w*i, hist_h),
-            (0, 0, 255), thickness=2)
+        cv2.line(histImage, (bin_w*i, hist_h - int(hist_atual[i])), (bin_w*i, hist_h), (0, 0, 255), thickness=2)
 
-    dif = cv2.compareHist(hist_anterior, hist_atual, cv2.HISTCMP_BHATTACHARYYA) # cv2.HISTCMP_BHATTACHARYYA retorna 0.0 para imagens iguais
+    dif = cv2.compareHist(hist_anterior, hist_atual, cv2.HISTCMP_BHATTACHARYYA)
     if (100 * dif > 3):
         cv2.putText(frame, "Movimento detectado!", (10, 30), cv2.FONT_HERSHEY_TRIPLEX, 1, (0, 0, 255))
     
