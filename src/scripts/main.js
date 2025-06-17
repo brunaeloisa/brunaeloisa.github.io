982b81db4f19f8c0f60f70ccf3e0844a6738ea1b
bruna <brunaeloisa7@gmail.com>
2025-06-17 20:56:31 -0300

    navegação e troca de tema

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,15 +1,29 @@
 const toggleThemeButton = document.getElementById('toggle-theme');
-const htmlElement = document.documentElement;
-const linkElements = document.querySelectorAll('.menu__link');
+const sectionElements = document.querySelectorAll('section');
+const isDesktop = window.innerWidth >= 768;
+const menuClass = isDesktop ? '.menu--desktop' : '.menu--mobile';
+const linkElements = document.querySelectorAll(`${menuClass} .menu__link`);
 
-function changeTheme() {
-  const currentTheme = htmlElement.dataset.theme;
-  htmlElement.dataset.theme = (currentTheme === 'dark' ? 'light' : 'dark');
-  toggleThemeButton.classList.toggle('bi-sun');
-  toggleThemeButton.classList.toggle('bi-moon-stars');
+// carrega tema do local storage se houver (default dark)
+let theme = localStorage.getItem('theme') || 'dark';
+
+function initToggleButton() {
+  const initialButtonIcon = (theme === 'dark' ? 'bi-sun' : 'bi-moon-stars');
+  toggleThemeButton.classList.add(initialButtonIcon);
+  toggleThemeButton.addEventListener('click', changeTheme);
 }
 
-toggleThemeButton.addEventListener('click', changeTheme);
+function updateTheme() {
+  document.documentElement.dataset.theme = theme;
+  localStorage.setItem('theme', theme);
+}
+
+function changeTheme() {
+  theme = (theme === 'dark' ? 'light' : 'dark');
+  toggleThemeButton.classList.toggle('bi-sun');
+  toggleThemeButton.classList.toggle('bi-moon-stars');
+  updateTheme();
+}
 
 function clearActive() {
   linkElements.forEach((link) => {
@@ -17,9 +31,41 @@ function clearActive() {
   });
 }
 
-linkElements.forEach((link) => {
-  link.addEventListener('click', () => {
-    clearActive();
-    link.classList.add('active');
+function initMenu() {
+  let ignoreScroll = false;
+  let scrollTimeOut = null;
+
+  linkElements.forEach((link) => {
+    link.addEventListener('click', () => {
+      clearTimeout(scrollTimeOut);
+      ignoreScroll = true;
+
+      clearActive();
+      link.classList.add('active');
+      scrollTimeOut = setTimeout(() => ignoreScroll = false, 1000);
+    });
   });
-});
+
+  window.addEventListener('scroll', () => {
+    if (ignoreScroll) return;
+    const scrollPosition = Math.ceil(window.scrollY) + 50;
+
+    sectionElements.forEach(section => {
+      const sectionPosition = section.offsetTop;
+      const sectionHeight = section.offsetHeight;
+
+      if (scrollPosition >= sectionPosition && scrollPosition < (sectionPosition + sectionHeight)) {
+        clearActive();
+        document.querySelector(`${menuClass} a[href="#${section.id}"]`).classList.add('active');
+      }
+    });
+  });
+}
+
+// atualiza a página para mudar o menu
+window.onresize = () => ((window.innerWidth >= 768) != isDesktop) && location.reload();
+window.onbeforeunload = () => window.scrollTo(0, 0);
+
+updateTheme();
+initToggleButton();
+initMenu();
